ecf446c80b77005da9a32761d1652c80eaaf0d23
Haxu <nguyenngocminh190901@gmail.com>
2021-12-16 15:26:27 +0700

    them dao-dien.html

--- js/script-dao-dien.js
+++ js/script-dao-dien.js
@@ -1,5 +1,9 @@
 $(document).ready(function () {
     
+    $(".dao-dien-link").each(function() {
+        $("[id='" + this.id + "'] .dao-dien-image-cover > img").attr("src", daoDienList[this.id].image);
+        $("[id='" + this.id + "'] .dao-dien-text").append("<h3>" + daoDienList[this.id].name + "</h3>")
+        
+    })
     
-
 });
