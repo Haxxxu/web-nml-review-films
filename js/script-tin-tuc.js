e76e68822753efadacd61851a53d0eef279db053
Haxu <nguyenngocminh190901@gmail.com>
2021-12-18 09:18:20 +0700

    thêm lại cái tin-tuc ?

---
+++ js/script-tin-tuc.js
@@ -0,0 +1,4 @@
+
+function updateChiTietPage(id) {
+    window.localStorage.setItem("tin-tuc-id", id);
+}
--- js/tin-tuc.js
+++
@@ -1,6 +0,0 @@
-$(document).ready(function(){
-    showtintuc(window.localStorage.getItem("tin-tuc-id"));
-});
-function showtintuc(id){
-    $("name").append(tintucList[id].name);
-};
